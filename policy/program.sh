93539111eb064423eacfb54abf7a75adbaac0a39
Yvonne Serota <yvonne.serota@cyberark.com>
2019-04-23 18:31:09 +0300

    Commit changes to the docker-compose yml and move myApp.yml to the policy folder

---
+++ policy/program.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Conjur server URL
+CONJUR_APPLIANCE_URL=http://10.0.68.207:8080
+main() {
+  CONT_SESSION_TOKEN=$(cat conjur_token| base64 | tr -d '\r\n')
+  VAR_VALUE=$(curl -s -k -H "Content-Type: application/json" -H "Authorization: Token token=\"$CONT_SESSION_TOKEN\"" $CONJUR_APPLIANCE_URL/secrets/myConjurAccount/variable/myApp%2FsecretVar)
+  echo "Your application is authenticated and the retrieved value is: $VAR_VALUE"
+}
+main "$@"
+exit
