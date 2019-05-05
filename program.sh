84ac61378f22be36902c76aff6d5adac630714ac
Yvonne Serota <yvonne.serota@cyberark.com>
2019-05-05 12:06:17 +0300

    Adding images to docker-compose and trmiming down the demo app

--- program.sh
+++ program.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Conjur server URL
-CONJUR_APPLIANCE_URL=http://10.0.68.207:8080
 main() {
-  CONT_SESSION_TOKEN=$(cat conjur_token| base64 | tr -d '\r\n')
-  VAR_VALUE=$(curl -s -k -H "Content-Type: application/json" -H "Authorization: Token token=\"$CONT_SESSION_TOKEN\"" $CONJUR_APPLIANCE_URL/secrets/myConjurAccount/variable/myApp%2FsecretVar)
+  CONT_SESSION_TOKEN=$(cat /tmp/conjur_token| base64 | tr -d '\r\n')
+  VAR_VALUE=$(curl -s -k -H "Content-Type: application/json" -H "Authorization: Token token=\"$CONT_SESSION_TOKEN\"" https://proxy/secrets/myConjurAccount/variable/BotApp%2FsecretVar)
   echo "The retrieved value is: $VAR_VALUE"
 }
 main "$@"
